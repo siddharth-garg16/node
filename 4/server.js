99874bdb3b7903a35f397e89ba9d41b98c32ad03
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-05-27 14:13:55 +0530

    Project4 - Simple web server using purely nodejs

---
+++ 4/server.js
@@ -0,0 +1,18 @@
+const http = require("http");//import http module for the server
+
+
+//step1 - create the server
+const server = http.createServer((request, response)=>{
+    console.log("New request received");
+    // console.log(request);
+    // console.log(response);
+})//callback function will be called everytime a new request hits the server
+
+
+//step2 - start the server
+const PORT = 3000;
+server.listen(PORT, "127.0.0.1", ()=>{
+    console.log(`Server listening to port ${PORT}`);
+})//callback function gets executed as soon as the server starts
+
+//everytime we hit 127.0.0.1:3000 in a new browser, "New request received will be printed"

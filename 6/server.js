e5b12d42a6d3db484cc8b4943d2dd4ac796bbbfd
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-05-28 23:04:22 +0530

    Project5 - handling json data and convert json into html in Nodejs

--- 6/server.js
+++ 6/server.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 
 let data = fs.readFileSync("./data/products.json", "utf-8");
 let products = JSON.parse(data);
+let productListHtml = fs.readFileSync("./product-list.html", "utf-8");
+
+let listPage = products.map((prod)=>{
+    let output = productListHtml.replace("{{%NAME%}}", prod.name);
+    output = output.replace("{{%PRICE%}}", prod.price);
+    return output;
+})
 
 const server = http.createServer((request, response)=>{
     console.log("New request recieved");
@@ -10,10 +17,13 @@ const server = http.createServer((request, response)=>{
     if(path==="/" || path.toLocaleLowerCase()==="/home"){
         response.end("Home");
     } else if(path.toLocaleLowerCase()==="/products") {
-        response.writeHead(200, {
-            'Content-Type':'application/json'
-        })
-        response.end(data);
+        // response.writeHead(200, {
+        //     'Content-Type':'application/json'
+        // })
+        // response.end(data);
+        
+        //-----------------json into html
+        response.end(listPage.join(','));
     } else {
         response.end("Error 404: Page not found");
     }

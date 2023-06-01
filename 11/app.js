70cea8ccde5ce50062701c651583b25f9ef935b4
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-06-01 23:09:30 +0530

    Project11 - understanding pipe method in nodejs

--- 11/app.js
+++ 11/app.js
@@ -4,3 +4,13 @@ const fs = require('fs');
 const server = http.createServer((request, response)=>{
     console.log("New request received");
 })
+
+server.listen(3000, "127.0.0.1", ()=>{
+    console.log('Server up and running');
+})
+
+server.on("request", (req, res)=>{
+    let rs = fs.createReadStream("./files/large-file.txt");
+
+    rs.pipe(res); //pass a writable stream in pipe (res in this case)
+})
